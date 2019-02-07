7c830d4b587b4c9f5899d86b242c9e516ff77782
LakhdeepKaur4 <lakhdeep.kaur@greatwits.com>
2019-02-07 12:58:54 +0530

    updated assets and assets type pagination

--- app/router/router.js
+++ app/router/router.js
@@ -178,7 +178,7 @@ module.exports = function(app) {
 
 	app.post('/api/assets', [authJwt.verifyToken], assetsController.create);
 
-	app.get('/api/assets/:page', [authJwt.verifyToken], assetsController.get);
+	app.get('/api/assets', [authJwt.verifyToken], assetsController.get);
 
 	app.put('/api/assets/:id', [authJwt.verifyToken], assetsController.update);
 
@@ -186,7 +186,7 @@ module.exports = function(app) {
 
 	app.post('/api/assetsType', [authJwt.verifyToken], assetsTypeController.create);
 
-	app.get('/api/assetsType/:page', [authJwt.verifyToken], assetsTypeController.get);
+	app.get('/api/assetsType/', [authJwt.verifyToken], assetsTypeController.get);
 
 	app.put('/api/assetsType/:id', [authJwt.verifyToken], assetsTypeController.update);
 
