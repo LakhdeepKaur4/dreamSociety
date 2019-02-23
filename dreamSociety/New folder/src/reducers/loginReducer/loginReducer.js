abb58a743356bb77ca572de6f5c3ca5d937be5fd
Siddharth Arora <siddharth.arora@greatwits.com>
2019-02-23 11:28:13 +0530

    final updates

--- dreamSociety/New folder/src/reducers/loginReducer/loginReducer.js
+++
@@ -1,9 +0,0 @@
-import {USER_LOGIN} from '../../actions/index'
-export default function(state={}, action){
-    switch(action.type){
-        case USER_LOGIN:
-            return {...state,error:action.payload};                                                                              
-        default:
-            return state;
-    }
-} 
