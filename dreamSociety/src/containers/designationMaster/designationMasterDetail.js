7acbd90ab3f2a0fca387cf8993bbf18ad53ca4db
mohitgreatwits <42135477+mohitgreatwits@users.noreply.github.com>
2019-03-08 16:29:10 +0530

    bug fixing

--- dreamSociety/src/containers/designationMaster/designationMasterDetail.js
+++ dreamSociety/src/containers/designationMaster/designationMasterDetail.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {  getDesignation, deleteDesignation, updateDesignation, deleteSelectDesignation } from './../../actionCreators/designationMasterAction';
+import { getDesignation, deleteDesignation, updateDesignation, deleteSelectDesignation } from './../../actionCreators/designationMasterAction';
 import { bindActionCreators } from 'redux';
 import SearchFilter from '../../components/searchFilter/searchFilter';
 import UI from '../../components/newUI/superAdminDashboard';
@@ -13,11 +13,11 @@ class DesignationMasterDetail extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            filterName:"designationName",
+            filterName: "designationName",
             editDesignationData: {
                 designationName: '',
                 designationId: '',
-                
+
                 isActive: false,
 
             },
@@ -28,8 +28,8 @@ class DesignationMasterDetail extends Component {
             errors: {},
             isDisabled: true,
             ids: [],
-         
-            CopyData:[]
+
+            CopyData: []
 
 
         };
@@ -52,12 +52,12 @@ class DesignationMasterDetail extends Component {
         this.setState({
             designationId,
             designationName,
-           
+
             modal: !this.state.modal
         })
     }
 
-    
+
 
 
     toggleModal = () => {
@@ -72,48 +72,48 @@ class DesignationMasterDetail extends Component {
 
     refreshData() {
         this.props.getDesignation().then(() => this.setState({ loading: false }))
-       
+
     }
 
 
     editdesignationName = () => {
-       
+
         const { designationId, designationName } = this.state
-        
+
         let errors = {};
-        if(this.state.designationName===''){
-            errors.designationName="designationName can't be empty"
+        if (this.state.designationName === '') {
+            errors.designationName = "designationName can't be empty"
         }
-        this.setState({errors});
+        this.setState({ errors });
         const isValid = Object.keys(errors).length === 0
-        
+
         if (isValid) {
             this.setState({
                 loading: true
             })
-        this.props.updateDesignation(designationId, designationName)
-            .then(() => this.refreshData())
-        this.setState({
-            editDesignationData: { designationId, designationName },
-            modal: !this.state.modal
-        })
-    }
+            this.props.updateDesignation(designationId, designationName)
+                .then(() => this.refreshData())
+            this.setState({
+                editDesignationData: { designationId, designationName },
+                modal: !this.state.modal
+            })
+        }
     }
 
-      deleteDesignationName = (designationId) => {
+    deleteDesignationName = (designationId) => {
         let { isActive } = this.state.editDesignationData
         this.setState({ loading: true })
 
-        if(window.confirm('Are You Sure ?')){
-        this.props.deleteDesignation(designationId, isActive)
-            .then(() => this.refreshData())
-        this.setState({editDesignationData: { isActive: false } })
+        if (window.confirm('Are You Sure ?')) {
+            this.props.deleteDesignation(designationId, isActive)
+                .then(() => this.refreshData())
+            this.setState({ editDesignationData: { isActive: false } })
         }
-        else{
+        else {
             this.refreshData()
-          this.setState({editDesignationData: { isActive: false } })
+            this.setState({ editDesignationData: { isActive: false } })
         }
-      }
+    }
 
 
 
@@ -123,91 +123,91 @@ class DesignationMasterDetail extends Component {
 
     searchFilter = (search) => {
         return function (x) {
-         
+
             return x.designationName.toLowerCase().includes(search.toLowerCase())
                 || !search;
         }
     }
 
-    deleteSelected(ids){
-        this.setState({loading:true,  isDisabled:true});
+    deleteSelected(ids) {
+        this.setState({ loading: true, isDisabled: true });
 
-        if(window.confirm('Are You Sure ?')){
-        this.props.deleteSelectDesignation(ids)
-        .then(() => this.refreshData())
-        .catch(err => err.response.data.message);
+        if (window.confirm('Are You Sure ?')) {
+            this.props.deleteSelectDesignation(ids)
+                .then(() => this.refreshData())
+                .catch(err => err.response.data.message);
         }
-        else{
+        else {
             this.refreshData()
         }
     }
 
     selectAll = () => {
         let selectMultiple = document.getElementsByClassName('SelectAll');
-        let ar =[];
-            for(var i = 0; i < selectMultiple.length; i++){
-                    ar.push(parseInt(selectMultiple[i].value));
-                    selectMultiple[i].checked = true;
-            }
-            this.setState({ids: ar});
-            if(ar.length > 0){
-                this.setState({isDisabled: false});
-            }
+        let ar = [];
+        for (var i = 0; i < selectMultiple.length; i++) {
+            ar.push(parseInt(selectMultiple[i].value));
+            selectMultiple[i].checked = true;
+        }
+        this.setState({ ids: ar });
+        if (ar.length > 0) {
+            this.setState({ isDisabled: false });
+        }
     }
 
-    unSelectAll = () =>{
-        
+    unSelectAll = () => {
+
         let unSelectMultiple = document.getElementsByClassName('SelectAll');
         let allIds = [];
-        for(var i = 0; i < unSelectMultiple.length; i++){
-                unSelectMultiple[i].checked = false
+        for (var i = 0; i < unSelectMultiple.length; i++) {
+            unSelectMultiple[i].checked = false
         }
-        
-        this.setState({ids: [ ...allIds]});
-        if(allIds.length === 0){
-            this.setState({isDisabled: true});
+
+        this.setState({ ids: [...allIds] });
+        if (allIds.length === 0) {
+            this.setState({ isDisabled: true });
         }
-        
+
     }
-    
+
 
     renderDesignation = ({ designationResult }) => {
         if (designationResult) {
-          
-            return designationResult.designation.sort((item1,item2)=>{
+
+            return designationResult.designation.sort((item1, item2) => {
                 var cmprVal = (item1[this.state.filterName].localeCompare(item2[this.state.filterName]))
                 return this.state.sortVal ? cmprVal : -cmprVal;
             }).filter(this.searchFilter(this.state.search)).map((item, index) => {
-                   console.log(item)
-                
+                console.log(item)
+
                 return (
                     <tr key={item.designationId}>
-                      <td><input type="checkbox" name="ids" className="SelectAll" value={item.designationId}
-                         onChange={(e) => {
-                            const {designationId} = item
-                            if(!e.target.checked){
-                                document.getElementById('allSelect').checked=false;
-                                let indexOfId = this.state.ids.indexOf(designationId);
-                                if(indexOfId > -1){
-                                    this.state.ids.splice(indexOfId, 1);
+                        <td><input type="checkbox" name="ids" className="SelectAll" value={item.designationId}
+                            onChange={(e) => {
+                                const { designationId } = item
+                                if (!e.target.checked) {
+                                    document.getElementById('allSelect').checked = false;
+                                    let indexOfId = this.state.ids.indexOf(designationId);
+                                    if (indexOfId > -1) {
+                                        this.state.ids.splice(indexOfId, 1);
+                                    }
+                                    if (this.state.ids.length === 0) {
+                                        this.setState({ isDisabled: true });
+                                    }
                                 }
-                                if(this.state.ids.length === 0){
-                                    this.setState({isDisabled: true});
+                                else {
+
+                                    this.setState({ ids: [...this.state.ids, designationId] });
+
+                                    if (this.state.ids.length >= 0) {
+                                        this.setState({ isDisabled: false })
+                                    }
                                 }
-                            }
-                            else {
-   
-                                this.setState({ids: [...this.state.ids, designationId]});
-                                
-                                if(this.state.ids.length >= 0){
-                                    this.setState({isDisabled: false})
-                                }
-                            }
-                                
-                             }}/></td>
-                        <td>{index+1}</td>
+
+                            }} /></td>
+                        <td>{index + 1}</td>
                         <td>{item.designationName}</td>
-                        <td> 
+                        <td>
                             <Button color="success mr-2" onClick={this.toggle.bind(this, item.designationId, item.designationName)} >Edit</Button>
                             <Button color="danger" onClick={this.deleteDesignationName.bind(this, item.designationId)} >Delete</Button>
 
@@ -233,8 +233,8 @@ class DesignationMasterDetail extends Component {
     }
 
 
-    
-    onKeyPressHandler=(event)=> {
+
+    onKeyPressHandler = (event) => {
         const pattern = /^[a-zA-Z ]+$/;
         let inputChar = String.fromCharCode(event.charCode);
         if (!pattern.test(inputChar)) {
@@ -242,14 +242,14 @@ class DesignationMasterDetail extends Component {
         }
     }
 
-    close=()=>{
+    close = () => {
         return this.props.history.replace('/superDashBoard')
     }
 
     render() {
         console.log(this.props.DesignationMasterReducer)
         // console.log(this.state.CopyData)
-   
+
         // var nonSortedArray = ['hi', 'yo', 'whatup', 'bye', 'lol'];
         // var sortedArray = nonSortedArray.sort(function (a, b) {
         //       if (a < b) return -1;
@@ -263,30 +263,34 @@ class DesignationMasterDetail extends Component {
             <Table className="table table-bordered">
                 <thead>
                     <tr>
-                    <th>Select All<input className="ml-2"
-                    id="allSelect"
-                    type="checkbox" onChange={(e) => {
-                            if(e.target.checked) {
-                                this.selectAll();
+                        <th>Select All<input className="ml-2"
+                            id="allSelect"
+                            type="checkbox" onChange={(e) => {
+                                if (e.target.checked) {
+                                    this.selectAll();
+                                }
+                                else if (!e.target.checked) {
+                                    this.unSelectAll();
+                                }
                             }
-                            else if(!e.target.checked){
-                                this.unSelectAll();
-                            } 
-                        }  
-                    }/></th>
+                            } /></th>
                         <th>#</th>
-                        <th onClick={()=>{
-                             this.setState((state)=>{return {sortVal:!state.sortVal,
-                                filterName:'designationName'}});
-                        }}>Designation Position 
+                        <th onClick={() => {
+                            this.setState((state) => {
+                                return {
+                                    sortVal: !state.sortVal,
+                                    filterName: 'designationName'
+                                }
+                            });
+                        }}>Designation Position
                          <i class="fa fa-arrows-v" id="sortArrow" aria-hidden="true"></i></th>
-                       
+
                         <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
                     {this.renderDesignation(this.props.DesignationMasterReducer)}
-                
+
                 </tbody>
             </Table></div>
         return (
@@ -294,9 +298,9 @@ class DesignationMasterDetail extends Component {
 
                 <UI onClick={this.logout}>
                     <div className="w3-container w3-margin-top w3-responsive">
-                    <div style={{cursor:'pointer'}} className="close" aria-label="Close" onClick={this.close}>
+                        <div style={{ cursor: 'pointer' }} className="close" aria-label="Close" onClick={this.close}>
                             <span aria-hidden="true">&times;</span>
-                    </div>
+                        </div>
                         <div className="top-details">
                             <h3>Designation Details</h3>
                             <Button onClick={this.routeToAddNewDesignation} color="primary">Add Designation</Button>
@@ -304,8 +308,8 @@ class DesignationMasterDetail extends Component {
                         <SearchFilter type="text" value={this.state.search}
                             onChange={this.searchOnChange} />
 
-<Button color="danger" disabled={this.state.isDisabled} className="mb-3"
-        onClick={this.deleteSelected.bind(this, this.state.ids)}>Delete Selected</Button>
+                        <Button color="danger" disabled={this.state.isDisabled} className="mb-3"
+                            onClick={this.deleteSelected.bind(this, this.state.ids)}>Delete Selected</Button>
 
                         {!this.state.loading ? tableData : <Spinner />}
                         <Modal isOpen={this.state.modal} toggle={this.toggleModal}>
@@ -337,14 +341,14 @@ class DesignationMasterDetail extends Component {
 
 
 function mapStatToProps(state) {
-     console.log(state)
+    console.log(state)
     return {
         DesignationMasterReducer: state.DesignationMasterReducer
     }
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators({ getDesignation, deleteDesignation, updateDesignation,deleteSelectDesignation }, dispatch)
+    return bindActionCreators({ getDesignation, deleteDesignation, updateDesignation, deleteSelectDesignation }, dispatch)
 }
 
 export default connect(mapStatToProps, mapDispatchToProps)(DesignationMasterDetail);
--- dreamSociety/src/reducers/flatOwnerReducer/flatOwnerReducer.js
+++ dreamSociety/src/reducers/flatOwnerReducer/flatOwnerReducer.js
@@ -1,4 +1,4 @@
-import {ADD_FLAT_OWNER,GET_FLAT_OWNER} from '../../actions/index';
+import {ADD_FLAT_OWNER,GET_FLAT_OWNER,DELETE_MULTIPLE_OWNER,REMOVE_OWNER} from '../../actions/index';
 
 export default function (state={},action){
     console.log('7777777777777777777777777777777',action)
@@ -7,6 +7,10 @@ export default function (state={},action){
         return {...state ,ownerList:action.payload}
         case GET_FLAT_OWNER:
         return {...state ,owners:action.payload}
+        case DELETE_MULTIPLE_OWNER:
+        return {...state ,multiOwners:action.payload}
+        case REMOVE_OWNER:
+        return {...state ,deleteOwners:action.payload}
         default:
         return state;
     }
