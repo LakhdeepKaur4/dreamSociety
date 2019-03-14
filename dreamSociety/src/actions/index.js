eb93db609867a8cb38051f9006832cf414e2e9d2
mohitgreatwits <42135477+mohitgreatwits@users.noreply.github.com>
2019-03-14 17:07:35 +0530

    changes

--- dreamSociety/src/actions/index.js
+++ dreamSociety/src/actions/index.js
@@ -1,6 +1,6 @@
 // export  const URN = 'http://192.168.1.105:5000/api';
-export  const URN = 'http://192.168.1.103:8081/api';
-// export  const URN = 'https://dreamsociety.herokuapp.com/api';
+// export  const URN = 'http://192.168.1.103:8081/api';
+export  const URN = 'https://dreamsociety.herokuapp.com/api';
 export const PicURN= 'https://dreamsociety.herokuapp.com/'; 
 export const DocURN= 'https://dreamsociety.herokuapp.com/'; 
 export const UR= 'https://dreamsociety.herokuapp.com/';
