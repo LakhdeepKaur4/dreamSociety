030878032fc4cd6c299c9c5c33b36a6357702757
shubang22 <46522119+shubang22@users.noreply.github.com>
2019-03-22 19:25:20 +0530

    done

--- dreamSociety/src/actions/index.js
+++ dreamSociety/src/actions/index.js
@@ -1,8 +1,8 @@
 // export  const URN = 'http://192.168.1.105:5000/api';
 // export  const URN = 'http://192.168.1.115:8081/api';
 // export  const URN = 'https://mydreamsociety.herokuapp.com/api';
-export  const URN = 'https://dreamsociety.herokuapp.com/api';
-//    export  const URN = 'http://192.168.1.4:8081/api';
+// export  const URN = 'https://dreamsociety.herokuapp.com/api';
+   export  const URN = 'http://192.168.1.4:8081/api';
 export const PicURN= 'https://dreamsociety.herokuapp.com/'; 
 export const DocURN= 'https://dreamsociety.herokuapp.com/'; 
 export const UR= 'https://dreamsociety.herokuapp.com/';
