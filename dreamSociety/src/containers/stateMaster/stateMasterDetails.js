8767e4c21ee858b264d90e437608a7d647bfd5da
Abhi14dec <40384674+Abhi14dec@users.noreply.github.com>
2019-03-23 13:32:32 +0530

    abhinav updated

--- dreamSociety/src/containers/stateMaster/stateMasterDetails.js
+++ dreamSociety/src/containers/stateMaster/stateMasterDetails.js
@@ -14,7 +14,7 @@ class flatMasterDetails extends Component {
    constructor(props){
       super(props);
     this.state = {
-             ids:[],
+            ids:[],
             stateId:'',
             countryId:'',
             countryName:'',
