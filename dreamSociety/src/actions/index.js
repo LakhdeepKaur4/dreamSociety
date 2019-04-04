6536e2e408310fffe9c0a93108943c6c83fb31c2
priyavermapv <40385503+priyavermapv@users.noreply.github.com>
2019-04-04 14:23:18 +0530

    updated

--- dreamSociety/src/actions/index.js
+++ dreamSociety/src/actions/index.js
@@ -1,11 +1,12 @@
+// export  const URN = 'https://dreamsociety.herokuapp.com/api';
+// export  const PicURN = 'https://dreamsociety.herokuapp.com/';
+// export  const DocURN = 'https://dreamsociety.herokuapp.com/';
+// export  const UR = 'https://dreamsociety.herokuapp.com/';
+
 export  const URN = 'https://mydreamsociety.herokuapp.com/api';
 export  const PicURN = 'https://mydreamsociety.herokuapp.com/';
 export  const DocURN = 'https://mydreamsociety.herokuapp.com/';
 export  const UR = 'https://mydreamsociety.herokuapp.com/';
-// export  const URN = 'https://mydreamsociety.herokuapp.com/api';
-// export  const PicURN = 'https://mydreamsociety.herokuapp.com/';
-// export  const DocURN = 'https://mydreamsociety.herokuapp.com/';
-// export  const UR = 'https://mydreamsociety.herokuapp.com/';
 
 export const USER_LOGIN='user_login';
 export const GET_ROLES='get_roles';
