36ec97f8b3486104cfca155b65c2e7343cd22500
priyavermapv <40385503+priyavermapv@users.noreply.github.com>
2019-04-17 18:12:31 +0530

    updated

--- dreamSociety/src/actions/index.js
+++ dreamSociety/src/actions/index.js
@@ -15,9 +15,7 @@ export  const UR = 'http://ec2-18-222-154-60.us-east-2.compute.amazonaws.com:808
 // export  const URN = 'https://dreamsociety.herokuapp.com/api';
 // export  const PicURN = 'https://dreamsociety.herokuapp.com/';
 // export  const DocURN = 'https://dreamsociety.herokuapp.com/';
-// export  const UR = 'https://dreamsociety.herokuapp.com/';
-
-
+// export  const UR = 'https://dreamsociety.herokuapp.co
 
 
 export const USER_LOGIN='user_login';
