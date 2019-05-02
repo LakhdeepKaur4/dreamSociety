b966f1fe6ff355d0d1a48cfb7e791ec786f8754c
priyavermapv <40385503+priyavermapv@users.noreply.github.com>
2019-05-02 10:56:28 +0530

    updated

--- dreamSociety/src/actions/index.js
+++ dreamSociety/src/actions/index.js
@@ -1,17 +1,17 @@
-// export  const URN = 'http://ec2-18-222-154-60.us-east-2.compute.amazonaws.com:8081/api';
-// export  const PicURN = 'http://ec2-18-222-154-60.us-east-2.compute.amazonaws.com:8081/';
-// export  const DocURN = 'http://ec2-18-222-154-60.us-east-2.compute.amazonaws.com:8081/';
-// export  const UR = 'http://ec2-18-222-154-60.us-east-2.compute.amazonaws.com:8081/';
+export  const URN = 'http://ec2-18-222-154-60.us-east-2.compute.amazonaws.com:8081/api';
+export  const PicURN = 'http://ec2-18-222-154-60.us-east-2.compute.amazonaws.com:8081/';
+export  const DocURN = 'http://ec2-18-222-154-60.us-east-2.compute.amazonaws.com:8081/';
+export  const UR = 'http://ec2-18-222-154-60.us-east-2.compute.amazonaws.com:8081/';
 
 // export  const URN = 'https://mydreamsociety.herokuapp.com/api';
 // export  const PicURN = 'https://mydreamsociety.herokuapp.com/';
 // export  const DocURN = 'https://mydreamsociety.herokuapp.com/';
 // export  const UR = 'https://mydreamsociety.herokuapp.com/';
 
-export  const URN = 'https://dreamsociety.herokuapp.com/api';
-export  const PicURN = 'https://dreamsociety.herokuapp.com/';
-export  const DocURN = 'https://dreamsociety.herokuapp.com/';
-export  const UR = 'https://dreamsociety.herokuapp.com/'
+// export  const URN = 'https://dreamsociety.herokuapp.com/api';
+// export  const PicURN = 'https://dreamsociety.herokuapp.com/';
+// export  const DocURN = 'https://dreamsociety.herokuapp.com/';
+// export  const UR = 'https://dreamsociety.herokuapp.com/'
 
 export const USER_LOGIN='user_login';
 export const GET_ROLES='get_roles';
@@ -115,7 +115,7 @@ export const DELETE_LOCATION='delete_location';
 export const DELETE_DETAILS='delete-details';
 export const ADD_INVENTORY='add_inventory';
 export const GET_INVENTORY='get_inventory';
-export const UPDATE_INVENTORY='update_inventory';
+export const UPDATE_INVENTORY='update_inventory';    
 export const REMOVE_INVENTORY='REMOVE_INVENTORY';
 export const PlaceHolder='--Select--';
 export const ADD_MAINTENANCE ='add_maintenance';
