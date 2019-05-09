f5adc05eb56b30bb1a5dcebf610d2c170b252c12
Siddharth Arora <siddharth.arora@greatwits.com>
2019-05-09 10:55:34 +0530

    tenant

--- dreamSociety/src/actions/index.js
+++ dreamSociety/src/actions/index.js
@@ -1,17 +1,18 @@
-export  const URN = 'http://ec2-18-222-154-60.us-east-2.compute.amazonaws.com:8081/api';
-export  const PicURN = 'http://ec2-18-222-154-60.us-east-2.compute.amazonaws.com:8081/';
-export  const DocURN = 'http://ec2-18-222-154-60.us-east-2.compute.amazonaws.com:8081/';
-export  const UR = 'http://ec2-18-222-154-60.us-east-2.compute.amazonaws.com:8081/';
+// export  const URN = 'http://ec2-18-222-154-60.us-east-2.compute.amazonaws.com:8081/api';
+// export  const PicURN = 'http://ec2-18-222-154-60.us-east-2.compute.amazonaws.com:8081/';
+// export  const DocURN = 'http://ec2-18-222-154-60.us-east-2.compute.amazonaws.com:8081/';
+// export  const UR = 'http://ec2-18-222-154-60.us-east-2.compute.amazonaws.com:8081/';
 
-// export  const URN = 'http://localhost:8083/api';
-// export  const PicURN = 'http://localhost::8083/';
-// export  const DocURN = 'http://localhost::8083/';
-// export  const UR = 'http://localhost::8083/';
+
+export  const URN = 'http://dreamsociety.herokuapp.com/api';
+export  const PicURN = 'http://dreamsociety.herokuapp.com/';
+export  const DocURN = 'http://dreamsociety.herokuapp.com/';
+export  const UR = 'http://dreamsociety.herokuapp.com/';
 
 // export  const URN = 'http://mydreamsociety.herokuapp.com/api';
 // export  const PicURN = 'https://mydreamsociety.herokuapp.com/';
 // export  const DocURN = 'https://mydreamsociety.herokuapp.com/';
-// export  const UR = 'https://mydreamsociety.herokuapp.com/'
+// export  const UR = 'https://mydreamsociety.herokuapp.com/';
 
 
 export const USER_LOGIN='user_login';
